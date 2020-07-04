b98fef988b8f1d16b17db3ac27b4539b5dc43554
Peerapon Chaipat <kisi550d@gmail.com>
2020-07-04 13:46:47 +0700

    5. Complete

--- 62120501007-PEXAM-MDT112/62120501007-PEXAM-MDT112.ino
+++ 62120501007-PEXAM-MDT112/62120501007-PEXAM-MDT112.ino
@@ -1,18 +1,30 @@
+#include "LiquidCrystal_I2C.h"
+LiquidCrystal_I2C lcd(0x27, 16, 2); //lcd
 #include "CytronMotorDriver.h"
 CytronMD motor(PWM_PWM, 5, 3);   //motor
 
 void setup(){
 tone(8,800,400);
+
 Serial.begin(9600);
 Serial.println("Hello MDT");
+
 motor.setSpeed(255);
 Serial.println("Turn CCW");
 delay(2000);
 motor.setSpeed(0);
+
 motor.setSpeed(-255);
 Serial.println("Turn CW");
 delay(3000);
 motor.setSpeed(0);
+
+lcd.init();
+lcd.backlight();
+lcd.setCursor(3, 0);
+lcd.print("Hello MDT");
+delay(1000);
+lcd.clear();
 }
 
 void loop(){
